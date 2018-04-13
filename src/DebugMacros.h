c0262d6eec5145627f0e1a8ebfba190470ad91b3
jerabaul29 <jean.rblt@gmail.com>
2018-04-14 00:07:08 +0200

    seems all work (checking pre processed .i file

--- src/DebugMacros.h
+++ src/DebugMacros.h
@@ -9,15 +9,14 @@
 
 #if DEBUG
 #  define DEBINIT SERIAL_DEBUG.begin(9600); delay(10); SERIAL_DEBUG.println();
-#  define DEBPSTATUS DEBPMSG("Compiled __DATE__ __TIME__") DEBPMSG("Debug on SERIAL_DEBUG")
+#  define DEBPSTATUS DEBPMSG("Compiled " __DATE__ " " __TIME__) DEBPMSG("Debug on " DEBSHOW(SERIAL_DEBUG) )
 // #  define DEBPMSG(x) SERIAL_DEBUG.println(); SERIAL_DEBUG.print(F("D ")); SERIAL_DEBUG.println(F(x));
-#  define DEBPMSG(x) SERIAL_DEBUG.println(F("D x"));
-#  define DEBPVAR(x) SERIAL_DEBUG.print(F("D #x ")); debug_println(x);  // TODO: check if should be "D "#x" " or not
+#  define DEBPMSG(x) SERIAL_DEBUG.println(F("D " x));
+#  define DEBPVAR(x) SERIAL_DEBUG.print(F("D " #x " ")); debug_println(x);  // TODO: check if should be "D "#x" " or not
 // #  define DEBPWHERE SERIAL_DEBUG.print(F("__FILE__")); SERIAL_DEBUG.print(F(" line ")); SERIAL_DEBUG.println(F(__LINE__));
-#  define DEBPWHERE SERIAL_DEBUG.println(F("D __FILE__ line __LINE__"));
+#  define DEBPWHERE SERIAL_DEBUG.print(F("D " __FILE__ " l " DEBSHOW(__LINE__)));
 // #  define DEBPMACRO(x) SERIAL_DEBUG.println(); SERIAL_DEBUG.println(F("D " #x " " DEBSHOW(x)));
-#  define DEBPMACRO(x) SERIAL_DEBUG.println(F("D #x DEBSHOW(x)"));
-#  define DEBSHOW(x) DEBSHOW_(x)
+#  define DEBPMACRO(x) SERIAL_DEBUG.println(F("D " #x " " DEBSHOW(x)));
 #else
 #  define DEBINIT // nothing
 #  define DEBPSTATUS // nothing
@@ -25,9 +24,9 @@
 #  define DEBPVAR(x) // nothing
 #  define DEBPWHERE // nothing
 #  define DEBPMACRO(x) // nothing
-#  define DEBSHOW(x) // nothing
 #endif
 
+#  define DEBSHOW(x) DEBSHOW_(x)
 #define DEBSHOW_(x) #x  // # preprocessor statement prevents expansion of operand; add a layer of indirection
 
 template<typename T>
